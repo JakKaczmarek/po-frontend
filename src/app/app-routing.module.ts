a4b641b88dad6ae5929ef9dce79fb90d539736e1
Jakub Kaczmarek <jakub.kaczmarek1114@gmail.com>
2024-12-28 20:07:52 +0100

    added first components with routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListOffersComponent } from './job-offers/list-offers/list-offers.component';
+import { CreateOfferComponent } from './job-offers/create-offer/create-offer.component';
+import { OfferDetailsComponent } from './job-offers/offer-details/offer-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Kiedy wchodzimy na '/', chcemy przekierować na '/login'
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Logowanie, rejestracja
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // Oferty pracy
+  { path: 'offers', component: ListOffersComponent },
+  { path: 'offers/new', component: CreateOfferComponent },
+  { path: 'offers/:id', component: OfferDetailsComponent },
+
+  // (opcjonalnie) 404 page
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListOffersComponent } from './job-offers/list-offers/list-offers.component';
+import { CreateOfferComponent } from './job-offers/create-offer/create-offer.component';
+import { OfferDetailsComponent } from './job-offers/offer-details/offer-details.component';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListOffersComponent,
+    CreateOfferComponent,
+    OfferDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
